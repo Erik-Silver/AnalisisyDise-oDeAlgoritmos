1441916904838057beccea875528f6942b62db63
Erik-Silver <grubblue2343@gmail.com>
2020-09-28 23:16:16 -0500

    correcciones menores de ortografia

--- PideNumero.py
+++ PideNumero.py
@@ -5,7 +5,7 @@ def main():
     print("\n QUE PERMITE VISUALIZAR LA TABLA DE MULTIPLICAR")
     print("\n DEL NUMERO QUE TU INDIQUES")
 
-    a = int(input("\n\nUsuario introduce el numero de la tabla que deseas: "))
+    a = int(input("\n\nUsuario introduce el numero de la tabla que deseas (1- 10): "))
 
     for i in range(1,1000001):
         print(f" {a} x {i} = {a*i}")
